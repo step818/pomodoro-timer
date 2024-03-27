98a02a5384a8dc876dafb16559f7c5d5026685b0
Stephen Trewick <Stephent818@gmail.com>
2024-03-27 14:39:56 -0700

    adds timer toggle button

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,17 +1,32 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+import { Button } from "@chakra-ui/react";
 
 const Timer = () => {
+  const [isCounting, setIsCounting] = useState<boolean>(false);
   const [timeLeft, setTimeLeft] = useState<number>(25);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setTimeLeft(timeLeft - 1);
+      if (isCounting && timeLeft > 0) {
+        setTimeLeft(timeLeft - 1);
+      }
     }, 1000);
-
     return () => clearTimeout(timer);
   });
 
-  return <div>{timeLeft > 0 ? timeLeft : <span>Time's up!!</span>}</div>;
+  const toggleTimer = () => {
+    setIsCounting(!isCounting);
+  };
+
+  return (
+    <div>
+      {timeLeft > 0 ? timeLeft : <span>Time's up!!</span>}
+      <Button colorScheme="blue" onClick={toggleTimer}>
+        {!isCounting ? <span>Start</span> : <span>Pause</span>}
+      </Button>
+    </div>
+  );
 };
 
 export default Timer;
